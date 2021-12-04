a7d4d32a01547157d6a50d9154a35076e99d8ecf
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2021-12-04 14:41:38 +0100

    made minor changes.

--- Day1.py
+++ Day1.py
@@ -2,8 +2,15 @@
 Adding two digit number input.
 if user enters 39, the program does 3 + 9 = 12
 """
-num = int(input('Enter a two digit number you would like to add?'))
+num_ = input('Enter a two digit number you would like to add?')
+num = int(num_)
 a = num // 10  # To get the whole number i.e 3 in 3.9
 b = num % 10  # TO get the reminder i.e. 9 in 3.9
 result = a + b
 print(result)
+
+# OR
+a_ = int(num_[0])
+b_ = int(num_[1])
+result_ = a_ + b_
+print(result_)
