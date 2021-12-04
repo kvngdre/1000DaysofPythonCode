1e27d0263ff060aafcc6a861ceaa1a44bbf0ca56
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2021-12-05 00:56:28 +0100

    minor updates

--- Day1.py
+++ Day1.py
@@ -1,6 +1,5 @@
 """"
-Adding two digit number input.
-if user enters 39, the program does 3 + 9 = 12
+Starting at the basics
 """
 num_ = input('Enter a two digit number you would like to add?')
 num = int(num_)
