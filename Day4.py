5ca404b7925acdad2e2aa21361f86f1d8dd3277b
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2021-12-05 10:33:14 +0100

    updated Day 4

---
+++ Day4.py
@@ -0,0 +1,12 @@
+""""
+lists
+"""
+import random
+
+f = [[2, 3], [4, 5], [6, 7, 8]]
+print(len(f))
+print(f[1][1])
+# I learnt to access a nested list you have to access the row you want first then the element in the list.
+# Think of each list in a nested list as a row
+# Some people pass 13 meaning column 1 row 3.
+
